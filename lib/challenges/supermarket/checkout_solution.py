3c0bebe6ebb720320df27bb7021629a643aeeea2
occy88croud <octavio.delser@croud.com>
2023-06-21 19:36:23 +0100

    update

--- lib/challenges/supermarket/checkout_solution.py
+++ lib/challenges/supermarket/checkout_solution.py
@@ -1,62 +1,77 @@
-from __future__ import annotations
-
-from .constants import DISCOUNTS
-from .data_models import Basket
-from .utils import choose_max, validate_skus
-
-# pre-sort static discounts.
 
 
-def compute_discounts(skus: str) -> int:
-    """Computest the total price given the frequency of skus as a str.
-    Parameters
-    ----------
-    skus : str
-    A string containing the SKUs to be counted. Each SKU should be an uppercase
-    letter [A-Z].
+# noinspection PyUnusedLocal
+# skus = unicode string
 
-    Returns
-    -------
+"""
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
+"""
 
-    """
-    static_discounts = sorted(
-        [d for d in DISCOUNTS if not d.discount_compute_required], reverse=True
-    )
-    dynamic_discounts = [d for d in DISCOUNTS if d.discount_compute_required]
+PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40
+}
 
-    basket = Basket(skus)
-    checkout_value = 0
+DISCOUNTS = {
+    "A": {
+        "quantity": 3,
+        "price": 130
+    },
+    "B": {
+        "quantity": 2,
+        "price": 45
+    }
+}
 
-    while static_discounts or dynamic_discounts:
-        # compute dynamic values first
-        dynamic_discounts = sorted(
-            [d for d in dynamic_discounts if d.discount_met(basket)], reverse=True
-        )
-        static_discounts = [d for d in static_discounts if d.discount_met(basket)]
-        try:
-            best_discount = choose_max(dynamic_discounts, static_discounts)
-        except ValueError:
-            break
-        basket = best_discount.apply_discount(basket)
-        checkout_value += best_discount.cost
-    return basket.value + checkout_value
-    # basket value and total discounts applied
+GIVEAWAYS = {
+    "E": {
+        "quantity": 2,
+        "free_item": "B"
+    }
+}
 
 
-def checkout(skus: str) -> int:
-    """Compute skus checkout value given discounts
+def checkout(skus: str):
+    print(f"skus: {skus}")
+    skus_list = sorted(list(skus))
+    unique_skus = set(skus_list)
+    total = 0
 
-    Parameters
-    ----------
-    skus: string representing skus
-    ABBBDKEEEDJHHSKKKE
-
-    Returns
-    -------
-    int: > 0 or -1 for error.
-    """
-    try:
-        validate_skus(skus)
-    except TypeError:
+    if any([sku not in PRICES for sku in skus_list]):
         return -1
-    return compute_discounts(skus)
+
+    for sku in unique_skus:
+        print(f"current total: {total}")
+        number_of_item = skus_list.count(sku)
+        if sku in DISCOUNTS:
+            discount = DISCOUNTS[sku]
+            if "free_item" in discount:
+                free_item_index = discount["free_item"]
+                number_of_free_items = number_of_item // discount["quantity"]
+                if free_item_index in unique_skus:
+                    number_of_free_items = min(number_of_free_items, skus_list.count(free_item_index))
+                    total -= number_of_free_items * PRICES[free_item_index]
+                if free_item_index in DISCOUNTS:
+                    free_item_discount = DISCOUNTS[free_item_index]
+                    number_of_discounts_to_negate = skus_list.count(free_item_index) // free_item_discount["quantity"]
+                    total += number_of_discounts_to_negate * free_item_discount["price"]
+
+            else:    
+                while number_of_item >= discount["quantity"]:
+                    total += discount["price"]
+                    number_of_item -= discount["quantity"]
+        total += number_of_item * PRICES[sku]
+
+
+    return total
--- tests/lib/test_supermarket/test_supermarket.py
+++ tests/lib/test_supermarket/test_supermarket.py
@@ -1,119 +1,31 @@
 import pytest
 
-from lib.challenges.supermarket.checkout_solution import (
-    Basket,
-    checkout,
-    compute_discounts,
-    validate_skus,
-)
-from lib.challenges.supermarket.constants import Item, Items
+from solutions.CHK import checkout_solution
 
+def test_does_individual_checkout() -> None:
+    assert checkout_solution.checkout("A") == 50
+    assert checkout_solution.checkout("B") == 30
+    assert checkout_solution.checkout("C") == 20
+    assert checkout_solution.checkout("D") == 15
 
-def _get_sku_parametrization():
-    return (
-        "skus,expected",
-        [
-            ("A", Items.A.value.total_price),
-            ("AA", Items.A.value.total_price * 2),
-            ("AAAAA", 200),
-            ("AAAAAAAA", 200 + 130),
-            ("EE", 80),
-            ("BBEE", 40 * 2 + 30),
-            ("BBEEB", 40 * 2 + 45),
-            ("FFF", Items.F.value.total_price * 2),
-            ("STX", 45),
-            ("SSSZ", 65),
-            ("ZZZ", 45),
-            ("SSS", 45),
-            ("STXS", 62),
-            ("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 1592),
-        ],
-    )
+def test_does_checkout_with_multiple_items_no_discounts() -> None:
+    assert checkout_solution.checkout("ABCD") == 115
 
+def test_does_checkout_with_multiple_items_with_discounts() -> None:
+    assert checkout_solution.checkout("AAA") == 130
+    assert checkout_solution.checkout("BB") == 45
+    assert checkout_solution.checkout("AAAAAA") == 260
+    assert checkout_solution.checkout("AAAAA") == 230
+    assert checkout_solution.checkout("AAAA") == 180
+    assert checkout_solution.checkout("BBBB") == 90
+    assert checkout_solution.checkout("BBB") == 75
+    assert checkout_solution.checkout("BBBAA") == 175
 
-class TestItem:
-    def test_item_initialization(self):
-        item = Item("A", 50, 3)
-        assert item.key == "A"
-        assert item.price == 50
-        assert item.quantity == 3
+def test_handle_cross_referenced_discounts() -> None:
+    assert checkout_solution.checkout("EEB") == 80
+    assert checkout_solution.checkout("EEEB") == 120
+    assert checkout_solution.checkout("EEEEBB") == 160
+    assert checkout_solution.checkout("BEBEEE") == 160
 
-    def test_item_total_price(self):
-        item = Item("A", 50, 3)
-        assert item.total_price == 150
-
-    def test_item_construct(self):
-        item = Item("A", 50)
-        assert item.quantity == 1
-        item.construct(3)
-        assert item.quantity == 3
-        assert item.total_price == 150
-
-
-class TestBasket:
-    def test_basket_initialization(self):
-        basket = Basket("")
-        assert isinstance(basket.items, dict)
-        assert basket.value == 0
-
-    def test_basket_create(self):
-        basket = Basket("AAABBB")
-        assert len(basket.items) == 2
-        assert basket.items["A"].quantity == 3
-        assert basket.items["B"].quantity == 3
-
-    def test_basket_value(self):
-        basket = Basket("AAABBB")
-        assert basket.value == (
-            Items["A"].value.total_price * 3 + Items["B"].value.total_price * 3
-        )
-
-    def test_basket_subtraction(self):
-        basket1 = Basket("AAABBB")
-        basket2 = Basket("AAB")
-        basket1 -= basket2
-        assert basket1.items["A"].quantity == 1
-        assert basket1.items["B"].quantity == 2
-
-    def test_basket_subtraction_error(self):
-        basket1 = Basket("AAABBB")
-        basket2 = Basket("AABCC")
-        with pytest.raises(ValueError):
-            basket1 -= basket2
-
-
-class TestCHK:
-    @pytest.mark.parametrize(
-        "input_skus",
-        [
-            "a",
-            "1",
-            "abc",
-            "A B C",
-            "123",
-        ],
-    )
-    def test_validate_skus_invalid(self, input_skus):
-        # ARRANGE
-        # ACT
-        # ASSERT
-        with pytest.raises(TypeError):
-            validate_skus(input_skus)
-
-    @pytest.mark.parametrize("input_skus", ["AA", "AB", ""])
-    def test_validate_skus_valid(self, input_skus):
-        # ARRANGE
-        # ACT
-        # ASSERT
-        validate_skus(input_skus)
-
-    @pytest.mark.parametrize(*_get_sku_parametrization())
-    def test_compute_discounts(self, skus, expected):
-        assert compute_discounts(skus) == expected
-
-    @pytest.mark.parametrize(*_get_sku_parametrization())
-    def test_checkout(self, skus, expected):
-        assert checkout(skus) == expected
-
-    def test_checkout_err(self):
-        assert checkout("invalid") == -1
+def test_does_checkout_with_invalid_items() -> None:
+    assert checkout_solution.checkout("Z") == -1
