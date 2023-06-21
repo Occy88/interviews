58929422728860f6a9c418bbca70358a9519a622
occy88croud <octavio.delser@croud.com>
2023-06-21 19:44:26 +0100

    w

--- lib/challenges/supermarket/checkout_solution.py
+++ lib/challenges/supermarket/checkout_solution.py
@@ -1,170 +1,164 @@
+from collections import defaultdict
+
+
+prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 70,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 20,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 17,
+    'Y': 20,
+    'Z': 21,
+}
+
+special_offers = {
+    # Each tuple is (number of items, discounted price)
+    'A': [(5, 200), (3, 130)],
+    'B': [(2, 45)],
+    'H': [(10, 80), (5, 45)],
+    'K': [(2, 120)],
+    'P': [(5, 200)],
+    'Q': [(3, 80)],
+    'V': [(3, 130), (2, 90)],
+}
+
+group_discount_offers = {
+    # Match any combination of the skus in the key such that X of them gives Y price, value being (X, Y)
+    ('Z', 'S', 'T', 'Y', 'X'): (3, 45)
+}
+
+buy_x_get_y_free_offers = {
+    # Item to get free, number of items required for discount, num to get free
+    'E': ('B', 2, 1),
+    'F': ('F', 2, 1),
+    'N': ('M', 3, 1),
+    'R': ('Q', 3, 1),
+    'U': ('U', 3, 1),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-from collections import defaultdict
+def build_counts_by_sku(skus: str):
+    if not type(skus) == str:
+        raise ValueError
 
-# Individual default prices for each SKU
-# All SKUs in this map are assumed to be valid and vice-versa
-price_map = {
-    "A" : 50,
-    "B" : 30,
-    "C" : 20,
-    "D" : 15,
-    "E" : 40,
-    "F" : 10,
-    "G" : 20,
-    "H" : 10,
-    "I" : 35,
-    "J" : 60,
-    "K" : 70,
-    "L" : 90,
-    "M" : 15,
-    "N" : 40,
-    "O" : 10,
-    "P" : 50,
-    "Q" : 30,
-    "R" : 50,
-    "S" : 20,
-    "T" : 20,
-    "U" : 40,
-    "V" : 50,
-    "W" : 20,
-    "X" : 17,
-    "Y" : 20,
-    "Z" : 21
-}
+    counts_per_sku = defaultdict(int)
+    for sku in skus:
+        if sku in prices:
+            counts_per_sku[sku] += 1
+        else:
+            raise ValueError
+    return counts_per_sku
 
-# for each deal/tuple corresponding to a SKU this map takes the form of:
-# tuple[0]SKU for tuple[1]
-offer_map = {
-    "A" : [(5, 200) ,(3, 130)],
-    "B" : [(2, 45)],
-    "H" : [(10, 80), (5, 45)],
-    "K" : [(2, 120)],
-    "P" : [(5, 200)],
-    "Q" : [(3, 80)],
-    "V" : [(3, 130), (2, 90)],
-}
 
-# for each deal/tuple corresponding to a SKU this makes takes the form of:
-# tuple[0]SKU get tuple[1] tuple[2] free
-offer_map_free = {
-    "E" : [(2, 1, "B")],
-    "F" : [(2, 1, "F")],
-    "N" : [(3, 1, "M")],
-    "R" : [(3, 1, "Q")],
-    "U" : [(3, 1, "U")],
-}
+def run_buy_x_get_y_free_offers(counts_per_sku: dict) -> dict:
+    """
+    Run all buy x get y free offers, removing the free items from the count
+    for that item.
 
-r5_deal = ["Z", "Y", "S", "T", "X"]
+    Note - mutates the input counts
+    """
+    for item, offer in buy_x_get_y_free_offers.items():
+        count = counts_per_sku[item]
+        free_sku = offer[0]
+        num_required = offer[1]
+        num_given_free_per_occurence = offer[2]
+        if (free_sku == item):
+            # Special case for the sku giving itself for free
+            # offer applies in groups of (amount_to_trigger + amount_to_remove)
+            # TODO - does this work for Y > 1?
+            num_required = num_required + num_given_free_per_occurence
+        num_occurrences = count // num_required
+        num_free = num_given_free_per_occurence * num_occurrences
+        # Even if the offer fires, can't give more for free than are actually in the basket
+        counts_per_sku[free_sku] = max(
+            0,
+            counts_per_sku[free_sku] - num_free
+        )
+    return counts_per_sku
+
+
+def run_group_offers(counts_by_sku) -> int:
+    """
+    Run all group offers, removing SKUs used from the count. Return the subtotal for all offers applied.
+
+    Note: mutates the counts_by_sku passed in, removing all items used towards offers
+    """
+    subtotal = 0
+    for sku_list, offer_terms in group_discount_offers.items():
+        amount_required = offer_terms[0]
+        price = offer_terms[1]
+        total_count = sum(counts_by_sku[sku] for sku in sku_list)
+        total_offers = total_count // amount_required
+        total_items_to_remove = total_offers * amount_required
+        for sku in sku_list:
+            remaining = counts_by_sku[sku]
+            amount_to_remove = min(total_items_to_remove, remaining)
+            counts_by_sku[sku] -= amount_to_remove
+            total_items_to_remove -= amount_to_remove
+
+        subtotal += price * total_offers
+
+    return subtotal
+
+
+def run_x_for_y_offers(counts_per_sku):
+    """
+    Run all 'X skus for Y price' offers. Return the subtotal for all offers applied.
+
+    Note: mutates the counts_by_sku passed in, removing all items used towards offers
+    """
+    total = 0
+    for sku, count in counts_per_sku.items():
+        remaining = count
+        if special_offers_for_sku := special_offers.get(sku):
+            for special_offer in special_offers_for_sku:
+                num_items_required = special_offer[0]
+                discounted_price = special_offer[1]
+                num_of_discounts = remaining // num_items_required
+                total += num_of_discounts * discounted_price
+                remaining -= num_of_discounts * num_items_required
+        counts_per_sku[sku] = remaining
+    return total
+
+
+def total_for_items(counts_per_sku):
+    total = 0
+    for sku, count in counts_per_sku.items():
+        total += prices[sku] * count
+    return total
+
 
 def checkout(skus):
-    sku_freq = defaultdict(int)
-    # create frequency mapping logging how many times each SKU was seen in our input
-    # returns -1 if SKU not in our price_map
-    for sku in skus:
-        if sku not in price_map:
-            return -1
-        sku_freq[sku] += 1
+    # String will have a letter for each occurrence of the item
+    try:
+        counts_per_sku = build_counts_by_sku(skus)
+    except ValueError:
+        return -1
 
-    # apply bundle deal
-    # r5_freq = defaultdict(int)
-    total_checkout_value = 0
+    run_buy_x_get_y_free_offers(counts_per_sku)
+    total = run_group_offers(counts_per_sku)
+    total += run_x_for_y_offers(counts_per_sku)
+    total += total_for_items(counts_per_sku)
 
-    total_r5_items = 0
-    for sku, freq in sku_freq.items():
-        if sku in r5_deal:
-            # r5_freq[sku] = freq
-            total_r5_items += freq
-
-    total_r5_bundles = total_r5_items // 3
-    total_checkout_value = total_r5_bundles * 45
-
-    counter = total_r5_bundles * 3
-    while counter > 0:
-        for sku in r5_deal:
-            while sku_freq[sku] > 0 and counter > 0:
-                sku_freq[sku] -= 1
-                counter -= 1
-
-    # apply savings in the case of buy x to get y free
-    for sku, deals in offer_map_free.items():
-        get_sku_freq = sku_freq[sku]
-        for deal in deals:
-            x = deal[0]
-            y = deal[1]
-            y_sku = deal[2]
-            num_free = get_sku_freq // x
-            if sku == y_sku:
-                num_free = get_sku_freq // ( x +y)
-                sku_freq[y_sku] -= num_free
-            else:
-                if sku_freq[y_sku] >= num_free * y:
-                    sku_freq[y_sku] -= num_free * y
-                else:
-                    sku_freq[y_sku] = 0
-            get_sku_freq -= num_free * x
-
-    # calculate checkout fees with deals
-    for sku, sku_freq in sku_freq.items():
-        if sku in offer_map:
-            deals = offer_map[sku]
-            temp_sku_freq = sku_freq
-            for deal in deals:
-                group_quantity = deal[0]
-                group_cost = deal[1]
-                groups = temp_sku_freq // group_quantity
-                temp_sku_freq -= groups * group_quantity
-                total = groups * group_cost
-                total_checkout_value += total
-            total_checkout_value += temp_sku_freq * price_map[sku]
-        else:
-            total_checkout_value += sku_freq * price_map[sku]
-
-    return total_checkout_value
-
-def test_checkout_empty():
-    assert checkout("") == 0
-
-def test_checkout_valid_basic():
-    assert checkout("AAAA") == 180
-    assert checkout("ABCD") == 115
-    assert checkout("AAAABCD") == 245
-
-def test_checkout_r2_deals():
-    assert checkout("AAAAAAAA") == 330
-    assert checkout("AAAAAAAAA") == 380
-    assert checkout("AAAAAAAAAEE") == checkout("AAAAAAAAABEE")
-
-def test_checkout_invalid():
-    assert checkout("ABCZ1") == -1
-
-def test_checkout_r3_deals():
-    assert checkout("F") == 10
-    assert checkout("FF") == 20
-    assert checkout ("FFF") == 20
-    assert checkout ("FFFF") == 30
-    assert checkout ("FFFFFF") == 40
-
-def test_checkout_r4_deals():
-    assert checkout("QQQRRR") == 210
-    assert checkout("VVVVV") == 220
-    assert checkout("NNNM") == 120
-
-def test_checkout_r5_deals():
-    assert checkout("STXYZ") == 82
-    assert checkout("STXYZA") == 132
-
-test_checkout_empty()
-test_checkout_valid_basic()
-test_checkout_invalid()
-test_checkout_r2_deals()
-test_checkout_r3_deals()
-test_checkout_r4_deals()
-test_checkout_r5_deals()
-
-
-
-
-
+    return total
